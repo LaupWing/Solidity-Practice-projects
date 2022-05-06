77e950eba901c6b975ffa621bfbf7d8db3b6dbd1
LaupWing <loc_strict@hotmail.com>
2022-05-06 18:17:07 +0200

    118

--- udemy_kickstarter/contracts/Greeter.sol
+++ udemy_kickstarter/contracts/Greeter.sol
@@ -3,20 +3,12 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-contract Greeter {
-    string private greeting;
+contract Campaign {
+   address public manager;
+   uint public minimum_contribution;
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
-    }
-
-    function greet() public view returns (string memory) {
-        return greeting;
-    }
-
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
-    }
+   constructor(uint minimum){
+      manager = msg.sender;
+      minimum_contribution = minimum;
+   }
 }
