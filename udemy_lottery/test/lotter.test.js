afdbf15a373dcddf17ece7812a7315459741920c
LaupWing <loc_strict@hotmail.com>
2022-05-17 18:43:05 +0200

    test added: Should register minimum contribution

--- udemy_lottery/test/lotter.test.js
+++ udemy_lottery/test/lotter.test.js
@@ -4,10 +4,19 @@ const { ethers } = require("hardhat");
 describe("Lottery", function () {
    let lottery
    let deployer, user1, user2, users
+   const minimum = '200'
 
    beforeEach(async ()=>{
-      [deployer, user1, user2, ...users] = ethers.getSigners()
+      [deployer, user1, user2, ...users] = await ethers.getSigners();
       const LotteryFactory = await ethers.getContractFactory('Lottery')
-      lottery = LotteryFactory.deploy(200)
+      lottery = await LotteryFactory.deploy(minimum)
+   })
+
+   describe('Deployment', async ()=>{
+      it('Should register minimum contribution', async ()=>{
+         expect((await lottery.minimum()).toString()).to.equal(minimum)
+         console.log(deployer.address)
+         console.log(await lottery.manager())
+      })
    })
 })
