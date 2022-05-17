ed3a5e96d8d0586f941f2465f1ffdc0dc8101b5c
LaupWing <loc_strict@hotmail.com>
2022-05-17 18:34:03 +0200

    added(lottery.test.js): beforeach

--- udemy_lottery/test/lotter.test.js
+++ udemy_lottery/test/lotter.test.js
@@ -2,5 +2,12 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Lottery", function () {
-   
+   let lottery
+   let deployer, user1, user2, users
+
+   beforeEach(async ()=>{
+      [deployer, user1, user2, ...users] = ethers.getSigners()
+      const LotteryFactory = await ethers.getContractFactory('Lottery')
+      lottery = LotteryFactory.deploy(200)
+   })
 })
