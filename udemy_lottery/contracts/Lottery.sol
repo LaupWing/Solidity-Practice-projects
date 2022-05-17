5094ba9e64ef5c6f7ce2346527430b3fc0148c3b
LaupWing <loc_strict@hotmail.com>
2022-05-17 16:27:24 +0200

    added: Minimum fee

--- udemy_lottery/contracts/Lottery.sol
+++ udemy_lottery/contracts/Lottery.sol
@@ -5,4 +5,17 @@ import "hardhat/console.sol";
 
 contract Lottery {
    address public manager;
+   address[] public players;
+   uint256 public minimum;
+
+   constructor (uint _minimum){
+      manager = msg.sender;
+      minimum = _minimum;
+   }
+
+   function enter() public payable {
+      require(msg.value > minimum, 'Doesnt met the enter fee');
+
+      players.push(msg.sender);
+   }
 }
