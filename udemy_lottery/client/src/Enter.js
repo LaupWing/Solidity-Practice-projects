930303a4b9b1945806baba4c25d5d80134e859e4
LaupWing <loc_strict@hotmail.com>
2022-05-30 19:29:46 +0200

    base enter modal

--- udemy_lottery/client/src/Enter.js
+++ udemy_lottery/client/src/Enter.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 const Enter = ({contract}) => {
    const [minimum, setMinimum] = useState(0)
+   const [enterFee, setEnterFee] = useState(0)
    useEffect(()=>{
       const fetching = async ()=>{
          const _minimum = await contract.minimum()
@@ -10,14 +11,18 @@ const Enter = ({contract}) => {
       fetching()
    },[])
    return (
-      <div className='flex rounded m-2 space-x-1 items-center'>
-         <p>Minimum: {minimum}</p>
-         <input 
-            type="number"  
-            value={10} 
-            className="rounded border-2 border-yellow-500"
-         />
-         <button className='btn'>enter</button>
+      <div className='flex flex-col rounded m-2 items-start bg-yellow-400 p-2'>
+         <p className='text-yellow-100 text-sm my-1 uppercase font-bold'>Minimum: {minimum}</p>
+         <div className='flex'>
+            <input 
+               type="number"  
+               value={enterFee} 
+               onChange={e=>setEnterFee(e.target.value)}
+               step={100}
+               className="rounded border-2 border-yellow-500 focus:outline-none p-1 py-0.5"
+            />
+            <button className='btn'>enter</button>
+         </div>
       </div>
    )
 }
