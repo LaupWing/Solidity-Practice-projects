fe4a754dc1e094e739efb5d5c58bbb74350d6151
LaupWing <loc_strict@hotmail.com>
2022-05-31 17:16:13 +0200

    entees

--- udemy_lottery/client/src/Enter.js
+++ udemy_lottery/client/src/Enter.js
@@ -3,9 +3,15 @@ import React, { useEffect, useState } from 'react'
 const Enter = ({contract}) => {
    const [minimum, setMinimum] = useState(0)
    const [enterFee, setEnterFee] = useState(0)
+   const [entees, setEntees] = useState([])
+   const [alreadyEntered, setAlreadyEntered] = useState(false)
+
    useEffect(()=>{
       const fetching = async ()=>{
          const _minimum = await contract.minimum()
+         const _entees = await contract.getPlayers()
+
+         setEntees(_entees)
          setMinimum(Number(_minimum.toString()))
       }
       fetching()
@@ -22,20 +28,32 @@ const Enter = ({contract}) => {
 
    return (
       <div className='flex flex-col rounded m-2 items-start bg-yellow-400 p-2'>
-         <p className='text-yellow-100 text-sm my-1 uppercase font-bold'>Minimum: {minimum}</p>
-         <form 
-            className='flex'
-            onSubmit={enterLottery}
-         >
-            <input 
-               type="number"  
-               value={enterFee} 
-               onChange={e=>setEnterFee(e.target.value)}
-               step={100}
-               className="rounded border-2 border-yellow-500 focus:outline-none p-1 py-0.5"
-            />
-            <button className='btn'>enter</button>
-         </form>
+         {entees.length > 0  && <ul className='flex flex-col my-2'>
+            {entees.map(entee =>(
+               <li 
+                  key={entee}
+                  className='text-xs text-yellow-200 font-bold'
+               >
+                  {entee}
+               </li>
+            ))}
+         </ul>}
+         <div className='flex flex-col'>
+            <p className='text-yellow-100 text-sm my-1 uppercase font-bold'>Minimum: {minimum}</p>
+            {!alreadyEntered && <form 
+               className='flex'
+               onSubmit={enterLottery}
+            >
+               <input 
+                  type="number"  
+                  value={enterFee} 
+                  onChange={e=>setEnterFee(e.target.value)}
+                  step={100}
+                  className="rounded border-2 border-yellow-500 focus:outline-none p-1 py-0.5"
+               />
+               <button className='btn'>enter</button>
+            </form>}
+         </div>
       </div>
    )
 }
