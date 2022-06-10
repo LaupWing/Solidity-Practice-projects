59276956df23b701222e5093ae126535997b0cca
LaupWing <loc_strict@hotmail.com>
2022-06-10 19:34:03 +0200

    heheh

--- kickstarter2/backend/test/campaing.test.js
+++ kickstarter2/backend/test/campaing.test.js
@@ -1,4 +1,5 @@
 const { ethers } = require("hardhat")
+const { expect } = require("chai")
 
 describe('Campaign', ()=>{
    let campaign
@@ -6,9 +7,18 @@ describe('Campaign', ()=>{
    let factory
    const name = 'Fictive name lol'
    let deployer, user1, users
-
+   
    beforeEach(async ()=>{
       [deployer, user1, ...users] = await ethers.getSigners()
-      const Factory = await ethers.getContractFactory('CampaignFactory')
+      const Factory = await ethers.getContractFactory('CampaignFactory');
+      factory = await Factory.deploy(name, 100);
+      [campaignAddress] = await factory.getDeployedCampaigns()
+      campaign = await ethers.getContractAt('Campaign', campaignAddress)
+   })
+
+   describe('deployment', async ()=>{
+      it('Should track name and minimum contribution', async ()=>{
+         expect(await campaign.minimum_contribution()).to.equal(100)
+      })
    })
 })
