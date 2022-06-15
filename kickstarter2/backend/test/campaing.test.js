fb9a84124f06f72a14dca3b807dfc88890d6fefb
LaupWing <loc_strict@hotmail.com>
2022-06-15 16:55:00 +0200

    minimum_contribution

--- kickstarter2/backend/test/campaing.test.js
+++ kickstarter2/backend/test/campaing.test.js
@@ -7,18 +7,19 @@ describe('Campaign', ()=>{
    let factory
    const name = 'Fictive name lol'
    let deployer, user1, users
-   
+   const minimum_contribution = 100
+
    beforeEach(async ()=>{
       [deployer, user1, ...users] = await ethers.getSigners()
       const Factory = await ethers.getContractFactory('CampaignFactory');
-      factory = await Factory.deploy(name, 100);
+      factory = await Factory.deploy(name, minimum_contribution);
       [campaignAddress] = await factory.getDeployedCampaigns()
       campaign = await ethers.getContractAt('Campaign', campaignAddress)
    })
 
    describe('deployment', async ()=>{
       it('Should track name and minimum contribution', async ()=>{
-         expect(await campaign.minimum_contribution()).to.equal(100)
+         expect(await campaign.minimum_contribution()).to.equal(minimum_contribution)
          expect(await campaign.name()).to.equal(name)
       })
       it('Marks caller as the campaign manager', async ()=>{
