51e1390f58c98b9be9e91ec345284e384f18bd58
LaupWing <loc_strict@hotmail.com>
2022-06-16 17:58:52 +0200

    udpated

--- kickstarter2/frontend/pages/_app.js
+++ kickstarter2/frontend/pages/_app.js
@@ -1,7 +1,14 @@
+import { useState } from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+   const [account, setAccount] = useState(null)
+
+   const web3Handler = async () =>{
+      const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
+      setAccount(accounts[0])
+   }
+   return <Component {...pageProps} />
 }
 
 export default MyApp
