e5722c51ac5646b80d7b08ef94b094881355975d
LaupWing <loc_strict@hotmail.com>
2022-06-17 16:09:55 +0200

    react false

--- kickstarter2/frontend/pages/_app.js
+++ kickstarter2/frontend/pages/_app.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
@@ -7,7 +7,12 @@ function MyApp({ Component, pageProps }) {
    const web3Handler = async () =>{
       const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
       setAccount(accounts[0])
+      console.log(account)
+      console.log(accounts)
    }
+   useEffect(()=>{
+      web3Handler()
+   },[])
    return <Component {...pageProps} />
 }
 
