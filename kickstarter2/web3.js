b7a0865f7abb9c656ec92fcc90a521e4fa01c460
LaupWing <loc_strict@hotmail.com>
2022-06-20 16:03:46 +0200

    getSigner

--- kickstarter2/web3.js
+++ kickstarter2/web3.js
@@ -1,28 +1,13 @@
-const web3Handler = async () =>{
-   const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
-   setAccount(accounts[0])
+import { ethers, providers } from 'ethers'
+
+export const getSigner = ()=>{
+   let signer = null
    
-   window.ethereum.on('chainChanged', ()=>{
-      window.location.reload()
-   })
-
-   window.ethereum.on('accountsChanged', ()=>{
-      // setLoading(true)
-      web3Handler()
-   })
-
-   const provider = new ethers.providers.Web3Provider(window.ethereum)
-   const test0 = new providers.JsonRpcProvider('http://localhost:8545/')
-   console.log(window.ethereum)
-   const signer = provider.getSigner()
-   console.log(test0.getSigner())
-   console.log(signer)
-   loadContract(signer)
-}
-const loadContract = async (signer) =>{
-   const _contract = new ethers.Contract( CampaignFactoryAddress.address, CampaignFactoryAbi.abi, signer)
-   const test = await _contract.getDeployedCampaigns()
-   console.log(test)
-   console.log(_contract)
-   setContract(_contract)
-}
+   let provider
+   if(window){
+      provider = new ethers.providers.Web3Provider(window.ethereum)
+   }else{
+      provider = new providers.JsonRpcProvider('http://localhost:8545/')
+   }
+   signer = provider.getSigner()
+} 
