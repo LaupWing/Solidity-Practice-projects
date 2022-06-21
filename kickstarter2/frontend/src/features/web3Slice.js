fefbb48a282a71f578a4aa9a8af89ab4c289a593
LaupWing <loc_strict@hotmail.com>
2022-06-21 16:01:55 +0200

    updated

--- kickstarter2/frontend/src/features/web3Slice.js
+++ kickstarter2/frontend/src/features/web3Slice.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { ethers } from 'ethers'
 import CampaignFactoryAddress from '../../contractsData/campaignFactory-address.json'
 import CampaignFactoryAbi from '../../contractsData/campaignFactory.json'
 
@@ -12,22 +13,21 @@ export const fetchWeb3 = createAsyncThunk(
    'web3/fetchWeb3Status',
    async ()=>{
       console.log('lol')
-      const accounts = window.ethereum.request({method: 'eth_requestAccounts'})
+      const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
       // window.ethereum.on('chainChanged', ()=>{
-      //    window.location.reload()
-      // })
-      // window.ethereum.on('accountsChanged', ()=>{
-      //    web3Handler()
-      // })
-      
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
+         //    window.location.reload()
+         // })
+         // window.ethereum.on('accountsChanged', ()=>{
+            //    web3Handler()
+            // })
+            
+      const provider = await new ethers.providers.Web3Provider(window.ethereum)
       const signer = provider.getSigner()
       const contract = new ethers.Contract(CampaignFactoryAddress.address, CampaignFactoryAbi.abi, signer)
       const test = await contract.getDeployedCampaigns()
-      console.log(test)
+      console.log(contract)
       return {
-         account: accounts[0],
-         contract
+         account: accounts[0]
       }
    }
 )
@@ -40,9 +40,10 @@ export const web3Slice= createSlice({
       
    },
    extraReducers: (builder) =>{
-      builder.addCase(fetchWeb3.fulfilled, (state, {payload})=>{
-         state.contract = payload.contract
-         state.account = payload.account
+      builder.addCase(fetchWeb3.fulfilled, (state, action)=>{
+         // state.contract = payload.contract
+         console.log(action)
+         // state.account = payload.account
       }),
       builder.addCase(fetchWeb3.rejected, (state)=>{
          state.error = 'Failed to fetch web3'
