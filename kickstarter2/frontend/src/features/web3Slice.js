315a6cb1e1e4b024fe8a327de238bf24872d0583
LaupWing <loc_strict@hotmail.com>
2022-06-21 15:39:45 +0200

    upadted

--- kickstarter2/frontend/src/features/web3Slice.js
+++ kickstarter2/frontend/src/features/web3Slice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
    contract: null,
@@ -6,30 +6,47 @@ const initialState = {
    error: false
 }
 
+const fetchWeb3 = createAsyncThunk(
+   'web3/fetchWeb3Status',
+   async ()=>{
+      const accounts = window.ethereum.request({method: 'eth_requestAccounts'})
+      // window.ethereum.on('chainChanged', ()=>{
+      //    window.location.reload()
+      // })
+      // window.ethereum.on('accountsChanged', ()=>{
+      //    web3Handler()
+      // })
+      
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract( CampaignFactoryAddress.address, CampaignFactoryAbi.abi, signer)
+      
+      return {
+         account: accounts[0],
+         contract
+      }
+   }
+)
+
+
 export const web3Slice= createSlice({
    name: 'web3',
    initialState,
    reducers: {
-      setWeb3: (state)=>{
-         const accounts = window.ethereum.request({method: 'eth_requestAccounts'})
-         // window.ethereum.on('chainChanged', ()=>{
-         //    window.location.reload()
-         // })
-         // window.ethereum.on('accountsChanged', ()=>{
-         //    web3Handler()
-         // })
-
-         const provider = new ethers.providers.Web3Provider(window.ethereum)
-         const signer = provider.getSigner()
-         const contract = new ethers.Contract( CampaignFactoryAddress.address, CampaignFactoryAbi.abi, signer)
-         
-         state.account = accounts[0]
-         state.contract = contract
-      }
+      
+   },
+   extraReducers: (builder) =>{
+      builder.addCase(fetchWeb3.fulfilled, (state, {payload})=>{
+         state.contract = payload.contract
+         state.account = payload.account
+      }),
+      builder.addCase(fetchWeb3.rejected, (state)=>{
+         state.error = 'Failed to fetch web3'
+      })
    }
 })
 
 
-export const { setWeb3 } = web3Slice.actions
+// export const { setWeb3 } = web3Slice.actions
 
 export default web3Slice.reducer
