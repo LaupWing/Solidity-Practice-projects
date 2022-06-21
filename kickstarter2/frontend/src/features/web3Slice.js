ba42dc2e031a40bef7c45f37eeec5d5e87d3ed7c
LaupWing <loc_strict@hotmail.com>
2022-06-21 16:08:35 +0200

    updated

--- kickstarter2/frontend/src/features/web3Slice.js
+++ kickstarter2/frontend/src/features/web3Slice.js
@@ -9,45 +9,48 @@ const initialState = {
    error: false
 }
 
+
 export const fetchWeb3 = createAsyncThunk(
    'web3/fetchWeb3Status',
-   async ()=>{
+   async () => {
       console.log('lol')
-      const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
       // window.ethereum.on('chainChanged', ()=>{
-         //    window.location.reload()
-         // })
-         // window.ethereum.on('accountsChanged', ()=>{
-            //    web3Handler()
-            // })
-            
+      //    window.location.reload()
+      // })
+      // window.ethereum.on('accountsChanged', ()=>{
+      //    web3Handler()
+      // })
+
       const provider = await new ethers.providers.Web3Provider(window.ethereum)
       const signer = provider.getSigner()
       const contract = new ethers.Contract(CampaignFactoryAddress.address, CampaignFactoryAbi.abi, signer)
       const test = await contract.getDeployedCampaigns()
       console.log(contract)
       return {
-         account: accounts[0]
+         account: accounts[0],
+         contract
       }
    }
 )
 
 
-export const web3Slice= createSlice({
+export const web3Slice = createSlice({
    name: 'web3',
    initialState,
+   middleware: (getDefaultMiddleware) =>getDefaultMiddleware({serializableCheck: false,}),
    reducers: {
-      
+
    },
-   extraReducers: (builder) =>{
-      builder.addCase(fetchWeb3.fulfilled, (state, action)=>{
+   extraReducers: (builder) => {
+      builder.addCase(fetchWeb3.fulfilled, (state, action) => {
          // state.contract = payload.contract
          console.log(action)
          // state.account = payload.account
       }),
-      builder.addCase(fetchWeb3.rejected, (state)=>{
-         state.error = 'Failed to fetch web3'
-      })
+         builder.addCase(fetchWeb3.rejected, (state) => {
+            state.error = 'Failed to fetch web3'
+         })
    }
 })
 
