2ac50889d160597769ec445374e83b6092709505
LaupWing <loc_strict@hotmail.com>
2022-06-22 14:49:15 +0200

    ait

--- kickstarter2/frontend/src/features/web3Slice.js
+++ kickstarter2/frontend/src/features/web3Slice.js
@@ -1,4 +1,4 @@
-import { createSlice, getDefaultMiddleware } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { ethers } from 'ethers'
 import CampaignFactoryAbi from '../../contractsData/campaignFactory.json'
 import CampaignFactoryAddress from '../../contractsData/campaignFactory-address.json'
@@ -10,18 +10,38 @@ const initialState = {
 }
 
 
+export const fetchWeb3 = createAsyncThunk(
+   'web3/fetchWeb3Status',
+   async ()=>{
+      const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(CampaignFactoryAddress.address, CampaignFactoryAbi.abi, signer)
+
+      return {
+         account: accounts[0],
+         contract
+      }
+   }
+)
+
+
 export const web3Slice = createSlice({
    name: 'web3',
    initialState,
    reducers: {
       setWeb3: (state, action) => {
-         const provider = new ethers.providers.Web3Provider(window.ethereum)
-         const signer = provider.getSigner()
-         const contract = new ethers.Contract(CampaignFactoryAddress.address, CampaignFactoryAbi.abi, signer)
          console.log(contract)
          state.contract = contract
          state.account = action.payload.account
       }
+   },
+   extraReducers: builder =>{
+      builder
+         .addCase(fetchWeb3.fulfilled, (state, action)=>{
+            state.account = action.payload.account
+            state.contract = action.payload.contract
+         })
    }
 })
 
