4fe4f55c0d84878827666797f90299447327cd54
LaupWing <loc_strict@hotmail.com>
2022-06-23 14:24:49 +0200

    added hooks

--- kickstarter2/frontend/src/pages/create-campaign.js
+++ kickstarter2/frontend/src/pages/create-campaign.js
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CreateCampaign = () => {
+   const [name, setName] = useState('')
+   const [minimum, setMinimum] = useState(0)
+
+   const handleSubmit = e =>{
+      e.preventDefault()
+      console.log(name)
+      console.log(minimum)
+   }
+
    return (
       <main className='w-full p-4 max-w-2xl mx-auto'>
-         <form className='w-full p-2 bg-white rounded shadow flex flex-col'>
+         <form 
+            className='w-full p-2 bg-white rounded shadow flex flex-col'
+            onSubmit={handleSubmit}
+         >
             <h2 className='text-slate-600 uppercase font-bold text-sm tracking-wider'>Create campaign</h2>
             <div className='w-full flex my-2'>
                <input 
                   type="text"  
-                  className='border border-slate-400 p-0.5 px-2 rounded flex-1'
+                  className='focus:outline-none border border-slate-400 p-0.5 px-2 rounded flex-1'
                   placeholder='Campaign Name'
+                  value={name}
+                  onChange={e=>setName(e.target.value)}
                   required
                />
                <input 
                   type="number" 
-                  className='border ml-1 w-24 px-2 border-slate-400 rounded' 
+                  className='focus:outline-none border ml-1 w-24 px-2 border-slate-400 rounded' 
                   placeholder='1'
+                  value={minimum}
+                  onChange={e=>setMinimum(e.target.value)}
                   required
                />
             </div>
