14105ba614fe05ee5811e622329d5acbb408d461
LaupWing <loc_strict@hotmail.com>
2022-06-23 14:58:37 +0200

    CampaingCard

---
+++ kickstarter2/frontend/src/components/CampaignCard/index.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react'
+
+const CampaignCard = ({address}) => {
+   useEffect(()=>{
+
+   },[])
+
+   return (
+      <div>CampaignCard</div>
+   )
+}
+
+export default CampaignCard
