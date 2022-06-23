f8188a22dff13cb3e2384fb3c66897c8ea5257fd
LaupWing <loc_strict@hotmail.com>
2022-06-23 15:53:38 +0200

    button

--- kickstarter2/frontend/src/pages/index.js
+++ kickstarter2/frontend/src/pages/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import {useSelector} from 'react-redux'
 import CampaignCard from '../components/CampaignCard'
+import Link from 'next/link'
 
 export default function Home() {
    const {contract} = useSelector(state => state.web3)
@@ -17,6 +18,9 @@ export default function Home() {
 
    return (
       <main className="p-4 w-full max-w-lg mx-auto space-y-4">
+         <Link href={`/create-campaign`}>
+            <button className='text-xs uppercase text-white bg-indigo-600 py-1 px-4 font-bold rounded'>Create</button>
+         </Link>
          {campaigns.map(c=>(
             <CampaignCard 
                address={c}
