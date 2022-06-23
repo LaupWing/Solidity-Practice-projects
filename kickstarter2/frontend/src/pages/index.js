80817b31cf0104d2162b614d1c9946b5f6a500ca
LaupWing <loc_strict@hotmail.com>
2022-06-23 14:38:16 +0200

    gettin ctract works

--- kickstarter2/frontend/src/pages/index.js
+++ kickstarter2/frontend/src/pages/index.js
@@ -1,4 +1,18 @@
-export default function Home(props) {
+import { useEffect } from 'react'
+import {useSelector} from 'react-redux'
+
+export default function Home() {
+   const {contract} = useSelector(state => state.web3)
+
+   useEffect(()=>{
+      const test = async ()=>{
+         const _t = await contract.getDeployedCampaigns()
+         console.log(_t)
+      }
+      test()
+   },[])
+
+
    return (
       <main className="p-4">
          <h2>Create Campaign</h2>
