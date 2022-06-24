1e8871f50a644fa10c0cdd91a15e16945622929c
LaupWing <loc_strict@hotmail.com>
2022-06-24 14:58:42 +0200

    setLoading

--- kickstarter2/frontend/src/pages/campaign/[address].js
+++ kickstarter2/frontend/src/pages/campaign/[address].js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 import CampaignAbi from '../../../contractsData/campaign.json'
 import {ethers} from 'ethers'
 import { useSelector } from 'react-redux'
+import ReactLoading from 'react-loading'
 
 const CampaignDetail = () => {
    const router = useRouter()
@@ -10,12 +11,14 @@ const CampaignDetail = () => {
    const {signer, account} = useSelector(state=>state.web3)
    const [owner, setOwner] = useState(false)
    const [name, setName] = useState('')
+   const [loading, setLoading] = useState(true)
 
    const fetchContract = async ()=>{
       const contract = new ethers.Contract(address, CampaignAbi.abi, signer)
       const manager = await contract.manager()
       setName(await contract.name())
       setOwner(ethers.utils.getAddress(account) === ethers.utils.getAddress(manager))
+      setLoading(false)
    }
 
    useEffect(()=>{
@@ -23,7 +26,13 @@ const CampaignDetail = () => {
    },)
 
    return (
-      <div>CampaignDetail {name}</div>
+      loading ? 
+         <ReactLoading className='m-auto'/> : 
+         <div className='p-3'>
+            <h2 className='font-bold text-slate-600'>
+               {name}
+            </h2>
+         </div>
    )
 }
 
