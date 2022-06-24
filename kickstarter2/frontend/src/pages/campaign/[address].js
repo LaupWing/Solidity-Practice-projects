c4f7128c67a8be1eacdbff84bd6cb2d84d549440
LaupWing <loc_strict@hotmail.com>
2022-06-24 15:03:11 +0200

    title finished

--- kickstarter2/frontend/src/pages/campaign/[address].js
+++ kickstarter2/frontend/src/pages/campaign/[address].js
@@ -10,13 +10,16 @@ const CampaignDetail = () => {
    const {address} = router.query
    const {signer, account} = useSelector(state=>state.web3)
    const [owner, setOwner] = useState(false)
+   const [minimum, setMinimum] = useState(false)
    const [name, setName] = useState('')
    const [loading, setLoading] = useState(true)
 
    const fetchContract = async ()=>{
       const contract = new ethers.Contract(address, CampaignAbi.abi, signer)
       const manager = await contract.manager()
+      console.log((await contract.approversCount()).toString())
       setName(await contract.name())
+      setMinimum((await contract.minimum_contribution()).toString())
       setOwner(ethers.utils.getAddress(account) === ethers.utils.getAddress(manager))
       setLoading(false)
    }
@@ -28,10 +31,12 @@ const CampaignDetail = () => {
    return (
       loading ? 
          <ReactLoading className='m-auto'/> : 
-         <div className='p-3'>
-            <h2 className='font-bold text-slate-600'>
+         <div className='p-3 flex flex-col'>
+            <h2 className='font-bold text-slate-400 flex'>
                {name}
+               <span className='ml-auto text-slate-600'>{minimum}</span>
             </h2>
+
          </div>
    )
 }
