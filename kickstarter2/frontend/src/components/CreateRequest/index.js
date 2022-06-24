9b8c5995987699a9722beea7fe15045d717672db
LaupWing <loc_strict@hotmail.com>
2022-06-24 16:01:59 +0200

    modal

--- kickstarter2/frontend/src/components/CreateRequest/index.js
+++ kickstarter2/frontend/src/components/CreateRequest/index.js
@@ -4,8 +4,10 @@ import Modal from 'react-modal'
 const CreateRequest = () => {
    return (
       <Modal
+         className='max-w-lg flex-col flex my-4 p-2 rounded bg-slate-500 m-auto w-full'
          isOpen={true}
       >
+         <h2 className='text-slate-700 font-bold uppercase mb-2'>Create request</h2>
          <input 
             type="text" 
             className='border border-slate-300 rounded'
