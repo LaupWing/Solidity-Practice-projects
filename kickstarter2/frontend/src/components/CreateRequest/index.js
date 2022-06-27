d1dd742cbcc7ce69aaf2507956232f70e41ce18d
LaupWing <loc_strict@hotmail.com>
2022-06-27 16:44:41 +0200

    Base

--- kickstarter2/frontend/src/components/CreateRequest/index.js
+++ kickstarter2/frontend/src/components/CreateRequest/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import Modal from 'react-modal'
 
+
+// string memory _title,
+// string memory _description, 
+// uint _value, 
+// address _recipient
 const CreateRequest = () => {
    return (
       <Modal
@@ -8,16 +13,28 @@ const CreateRequest = () => {
          isOpen={true}
       >
          <h2 className='text-slate-700 font-bold uppercase mb-2'>Create request</h2>
-         <input 
-            type="text" 
-            className='input'
-            placeholder='name'
-         />
-         <input 
-            type="text" 
-            className='input'
-            placeholder='name'
-         />
+         <main className='flex flex-col space-y-2'>
+            <input 
+               type="text" 
+               className='input'
+               placeholder='Name'
+            />
+            <input 
+               type="text" 
+               className='input'
+               placeholder='Receipient address'
+            />
+            <input 
+               type="number" 
+               className='input'
+               placeholder='0'
+            />
+            <textarea 
+               className='p-1 rounded'
+               placeholder='Description'
+            ></textarea>
+            <button className='btn'>Create</button>
+         </main>
       </Modal>
    )
 }
