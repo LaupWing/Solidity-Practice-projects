828df222aab40d9f71dec20fa2dc988e27ebc45c
LaupWing <loc_strict@hotmail.com>
2022-06-27 16:55:59 +0200

    testing create request

--- kickstarter2/frontend/src/components/CreateRequest/index.js
+++ kickstarter2/frontend/src/components/CreateRequest/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Modal from 'react-modal'
 
 
@@ -6,35 +6,62 @@ import Modal from 'react-modal'
 // string memory _description, 
 // uint _value, 
 // address _recipient
-const CreateRequest = () => {
+const CreateRequest = ({createRequest}) => {
+   const [name, setName] = useState('')
+   const [address, setAddress] = useState('')
+   const [value, setValue] = useState('')
+   const [description, setDescription] = useState('')
+
+   const handleSubmit = async e =>{
+      e.preventDefault()
+      createRequest({
+         name,
+         address,
+         value,
+         description,
+      })
+   }
+
    return (
       <Modal
          className='max-w-lg flex-col flex my-4 p-2 rounded bg-slate-500 m-auto w-full'
+         ariaHideApp={false}
          isOpen={true}
       >
          <h2 className='text-slate-700 font-bold uppercase mb-2'>Create request</h2>
-         <main className='flex flex-col space-y-2'>
+         <form 
+            className='flex flex-col space-y-2'
+            onSubmit={handleSubmit}
+         >
             <input 
                type="text" 
                className='input'
                placeholder='Name'
-            />
+               value={name}
+               onChange={e=>setName(e.target.value)}
+               />
             <input 
                type="text" 
                className='input'
                placeholder='Receipient address'
-            />
+               value={address}
+               onChange={e=>setAddress(e.target.value)}
+               />
             <input 
                type="number" 
                className='input'
                placeholder='0'
-            />
+               value={value}
+               onChange={e=>setValue(e.target.value)}
+               />
             <textarea 
                className='p-1 rounded'
                placeholder='Description'
+               value={description}
+               onChange={e=>setDescription(e.target.value)}
             ></textarea>
             <button className='btn'>Create</button>
-         </main>
+         </form>
       </Modal>
    )
 }
--- kickstarter2/frontend/src/pages/campaign/[address].js
+++ kickstarter2/frontend/src/pages/campaign/[address].js
@@ -29,6 +29,17 @@ const CampaignDetail = () => {
       setLoading(false)
    }
 
+   const createRequest = async (request) =>{
+      const transation = await contract.createRequest(
+         request.name,
+         request.description,
+         request.value,
+         request.address
+      )
+      await transation.wait()
+      console.log(transation)
+   }
+
    useEffect(()=>{
       fetchContract()
    },[])
@@ -37,7 +48,9 @@ const CampaignDetail = () => {
       loading ? 
          <ReactLoading className='m-auto'/> : 
          <div className='p-3 flex flex-col'>
-            <CreateRequest/>
+            <CreateRequest
+               createRequest={createRequest}
+            />
             <h2 className='font-bold text-slate-400 flex'>
                {name}
                <span className='ml-auto text-slate-600'>{minimum}</span>
