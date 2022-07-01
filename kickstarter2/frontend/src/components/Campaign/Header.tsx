fe25b9e662d43c909ad12d367ebdcedf1232a6a9
LaupWing <loc_strict@hotmail.com>
2022-07-01 15:11:51 +0200

    CampaignHeader

---
+++ kickstarter2/frontend/src/components/Campaign/Header.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react'
+
+interface Props {
+   contract: Function
+}
+
+const CampaignHeader:FC<Props> = () => {
+   return (
+      <header>Header</header>
+   )
+}
+
+export default CampaignHeader
