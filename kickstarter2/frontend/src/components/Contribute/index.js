3d71838b7ce9fd30a56de2c91b0c60afe5c0c665
LaupWing <loc_strict@hotmail.com>
2022-07-05 16:47:42 +0200

    ai

--- kickstarter2/frontend/src/components/Contribute/index.js
+++ kickstarter2/frontend/src/components/Contribute/index.js
@@ -12,20 +12,18 @@ const Contribute = ({contract, minimum}) => {
 
       const transation = await contract.contribute({value: contribution})
       await transation.wait()
-      console.log(contract)
-      console.log(contribution)
    }
 
    return (
       <form 
-         className='my-4 flex'
+         className='my-4 flex border border-slate-300 mr-auto p-1 rounded'
          onSubmit={handleSubmit}
       >
          <input 
             type="number" 
             value={contribution}
             onChange={e=>setContribution(e.target.value)}
-            className='rounded mr-1 px-1 w-40'
+            className='rounded mr-1 px-1 w-40 bg-slate-200 focus:outline-none'
          />
          <button 
             className='btn'
