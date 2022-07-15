83bdbaa8e1374d0f3a837465f3e0f058534717ad
LaupWing <loc_strict@hotmail.com>
2022-07-15 14:57:37 +0200

    bassis header

--- kickstarter2/frontend/src/components/Layout/Header/index.js
+++ kickstarter2/frontend/src/components/Layout/Header/index.js
@@ -4,8 +4,13 @@ import {useSelector} from 'react-redux'
 const Header = () => {
    const {account} = useSelector(state => state.web3)
    return (
-      <header className='w-full flex p-2'>
-         <p className=' text-xs ml-auto text-slate-400 font-bold'>{account}</p>
+      <header className='w-full flex items-center px-6 py-4 border-b border-slate-200'>
+         <nav className='basis-1/3 flex'>
+            <p>Discover</p>
+            <p>Create</p>
+         </nav>
+         <h1 className='uppercase font-bold basis-1/3 text-center'>kickstarter</h1>
+         <p className='basis-1/3 text-xs text-right ml-auto text-slate-400 font-bold'>{account}</p>
       </header>
    )
 }
