d136061fae0281949a37a60e589a27e410236350
LaupWing <loc_strict@hotmail.com>
2022-07-18 14:50:16 +0200

    removed old code

--- kickstarter2/frontend/src/components/Campaign/Header.js
+++ kickstarter2/frontend/src/components/Campaign/Header.js
@@ -16,15 +16,6 @@ const CampaignHeader = ({
       <header className='flex flex-col text-slate-400 leading-5'>
          <h2 className='text-center font-bold text-xl'>{name}</h2>
          <p className='text-center my-4'>{description}</p>
-         {/* <h2>
-            <span className='font-bold'>Minimum (Ether): </span>{minimum}
-         </h2>
-         <h2>
-            <span className='font-bold'>Balance (Ether): </span>{balance}
-         </h2>
-         <h2>
-            <span className='font-bold'>Contributors: </span>{contributors}
-         </h2> */}
          <div className='flex items-start'>
             <div 
                className='w-3/5 relative'
