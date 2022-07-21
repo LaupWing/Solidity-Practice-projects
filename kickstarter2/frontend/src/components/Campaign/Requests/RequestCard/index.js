215d868c4a54e19289ef1709df040380031adea4
LaupWing <loc_strict@hotmail.com>
2022-07-21 15:51:58 +0200

    fixed some styling

--- kickstarter2/frontend/src/components/Campaign/Requests/RequestCard/index.js
+++ kickstarter2/frontend/src/components/Campaign/Requests/RequestCard/index.js
@@ -11,12 +11,14 @@ const RequestCard = ({
 
    return (
       <div className='border border-slate-300 p-2 flex flex-col'>
-         <h3 className='flex ml-auto items-center rounded-full px-4 py-0.5 bg-slate-200 text-md text-slate-500'>
-            {request.value} ETH<FaEthereum/>
-         </h3>
+         <div className='flex justify-between'>
+            <h3 className='flex ml-auto items-center rounded-full px-4 py-0.5 bg-slate-200 text-md text-slate-500'>
+               {request.value} ETH<FaEthereum/>
+            </h3>
+         </div>
          <h1 className='font-bold text-slate-500'>{request.title}</h1>
          <p className='text-slate-400'>{request.description}</p>
-         <div className='flex flex-col my-2'>
+         <div className='flex flex-col my-2 mt-auto'>
             <div className='w-full border-2 border-green-500 rounded-full p-0.5'>
                <div
                   className='bg-green-500 h-1.5 rounded-full'
@@ -27,6 +29,7 @@ const RequestCard = ({
 
                </div>
             </div>
+
             <div className='flex justify-between'>
                <p className='text-green-500 font-bold'>{request.approvalCount}/{contributors}</p>
                <p className='text-slate-400'>{(Number(contributors)/2) - Number(request.approvalCount)} left</p>
