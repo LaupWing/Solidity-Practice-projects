e989c8f818530d0304e3f6a01bd501738ebc642f
LaupWing <loc_strict@hotmail.com>
2022-07-21 15:54:00 +0200

    added recipient info

--- kickstarter2/frontend/src/components/Campaign/Requests/RequestCard/index.js
+++ kickstarter2/frontend/src/components/Campaign/Requests/RequestCard/index.js
@@ -8,11 +8,12 @@ const RequestCard = ({
    owner,
    approveRequest
 }) => {
-
+   console.log(request)
    return (
       <div className='border border-slate-300 p-2 flex flex-col'>
-         <div className='flex justify-between'>
-            <h3 className='flex ml-auto items-center rounded-full px-4 py-0.5 bg-slate-200 text-md text-slate-500'>
+         <div className='flex justify-between items-center'>
+            <p className='truncate text-sm text-slate-400'>{request.recipient}</p>
+            <h3 className='flex ml-auto shrink-0 items-center rounded-full px-4 py-0.5 bg-slate-200 text-md text-slate-500'>
                {request.value} ETH<FaEthereum/>
             </h3>
          </div>
