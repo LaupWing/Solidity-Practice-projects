5f0c1de4fa7d2f3dcbc302aca74aa3acf38e01cd
LaupWing <loc_strict@hotmail.com>
2022-07-26 16:42:31 +0200

    udpated

--- kickstarter2/frontend/src/components/Campaign/Requests/RequestCard/index.js
+++ kickstarter2/frontend/src/components/Campaign/Requests/RequestCard/index.js
@@ -34,18 +34,23 @@ const RequestCard = ({
    },[])
 
    return (
-      <div className='border border-slate-300 p-2 flex flex-col rounded bg-slate-50'>
+      <div className='border border-slate-300 p-2 flex flex-col rounded bg-slate-50 relative'>
+         {request.complete && <div className='absolute inset-0 bg-white bg-opacity-90 flex justify-center items-center text-slate-300 font-bold text-sm'>
+            <p>ALREADY SENDED</p>
+         </div>}
          <RequestHeader request={request}/>
          <RequestContent request={request}/>
          <RequestProgress request={request} contributors={contributors}/>
-         {owner ? ( 
+         {owner ? !request.complete ? ( 
             <button 
                className={'text-xs py-1 text-white uppercase px-2 rounded m-1 ' + `${Number(request.approvalCount) > (Number(contributors)/2) ? 'bg-blue-500' : 'bg-slate-300 cursor-not-allowed'}`}
                onClick={()=>sendRequest(index)}
             >
                Send
             </button>
-         ) : 
+            ) : (
+               <p className='text-xs py-1 font-bold text-slate-300 uppercase'>Request is already send</p>
+            ) : 
          ((!answered.approved && !answered.denied && !loading) ? <div className='flex w-full justify-between'>
             <button 
                className='text-xs bg-red-500 py-1 text-white uppercase px-2 rounded w-20'
