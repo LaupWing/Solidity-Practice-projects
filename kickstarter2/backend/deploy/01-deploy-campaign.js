ca793fd0070af9485156190d3bfcc9379bbffa34
LaupWing <loc_strict@hotmail.com>
2022-08-03 15:35:15 +0200

    deploy

---
+++ kickstarter2/backend/deploy/01-deploy-campaign.js
@@ -0,0 +1,5 @@
+module.exports = async({getNamedAccounts, deployments, network}) =>{
+   const {deploy, log} = deployments
+   const {deployer} = await getNamedAccounts()
+   const chainId = network.config.chainId
+}
