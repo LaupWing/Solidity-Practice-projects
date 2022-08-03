da42ec6b007e70c8d2664d601b490e68dd2a5f29
LaupWing <loc_strict@hotmail.com>
2022-08-03 15:16:13 +0200

    priceconverter

--- kickstarter2/backend/contracts/PriceConverter.sol
+++ kickstarter2/backend/contracts/PriceConverter.sol
@@ -3,3 +3,24 @@ pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+library PriceConverter{
+   function getPrice(AggregatorV3Interface pricefeed)
+      internal
+      view
+      returns (uint256)
+   {
+      (, int256 answer, , , ) = pricefeed.latestRoundData();
+
+      return uint256(answer * 10000000000);
+   }
+
+   function getConverionRate(uint256 ethAmount, AggregatorV3Interface pricefeed) 
+      internal
+      view
+      returns (uint256)
+   {
+      uint256 ethPrice = getPrice(pricefeed);
+      uint256 ethAmountInUsd = (ethPrice * ethAmount) / 10000000000;
+      return ethAmountInUsd;
+   }
+}
