46c3926393e917058753d6f8b18d54a7ef9936f5
LaupWing <loc_strict@hotmail.com>
2022-08-04 17:02:24 +0200

    ahaaa

--- kickstarter2/backend/hardhat.config.js
+++ kickstarter2/backend/hardhat.config.js
@@ -5,12 +5,18 @@ require("hardhat-deploy")
 
 module.exports = {
    solidity: {
-      compilers :[
-         {version:"0.8.8"},
-         {version:"0.6.6"}
+      compilers: [
+         { version: "0.8.8" },
+         { version: "0.6.6" }
       ]
    },
    gasReporter: {
       enabled: true
-   }
+   },
+   namedAccounts: {
+      deployer: {
+         default: 0, // here this will by default take the first account as deployer
+         1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+      },
+   },
 };
