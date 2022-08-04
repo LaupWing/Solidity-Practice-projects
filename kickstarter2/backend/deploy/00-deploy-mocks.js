a9be453a9e5d3e78a8d5e3a068f9854d9bcaad2e
LaupWing <loc_strict@hotmail.com>
2022-08-04 16:30:34 +0200

    updated

--- kickstarter2/backend/deploy/00-deploy-mocks.js
+++ kickstarter2/backend/deploy/00-deploy-mocks.js
@@ -1,21 +1,9 @@
 const { networkConfig } = require("../helper-hardhat-config")
 const {network} = require("hardhat")
 
+
 module.exports = async({getNamedAccounts, deployments}) =>{
    const {deploy, log} = deployments
    const {deployer} = await getNamedAccounts()
    const chainId = network.config.chainId
-   console.log(networkConfig)
-
-   const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
-   if(true){
-
-   }else{
-
-   }
-
-   const campaign = await deploy("CampaignFactory", {
-      from: deployer,
-      args: []
-   })
 }
