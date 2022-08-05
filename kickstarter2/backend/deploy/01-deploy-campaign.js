d7af0ab025e08ee11033c78c355b59ee277cd1bd
LaupWing <loc_strict@hotmail.com>
2022-08-05 15:08:29 +0200

    he

--- kickstarter2/backend/deploy/01-deploy-campaign.js
+++ kickstarter2/backend/deploy/01-deploy-campaign.js
@@ -1,21 +1,21 @@
 const { networkConfig } = require("../helper-hardhat-config")
 const {network} = require("hardhat")
 
-// module.exports = async({getNamedAccounts, deployments}) =>{
-//    const {deploy, log} = deployments
-//    const {deployer} = await getNamedAccounts()
-//    const chainId = network.config.chainId
-//    console.log(networkConfig)
+module.exports = async({getNamedAccounts, deployments}) =>{
+   const {deploy, log} = deployments
+   const {deployer} = await getNamedAccounts()
+   const chainId = network.config.chainId
+   console.log(networkConfig)
 
-//    const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
-//    if(true){
+   const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+   if(true){
 
-//    }else{
+   }else{
 
-//    }
+   }
 
-//    const campaign = await deploy("CampaignFactory", {
-//       from: deployer,
-//       args: []
-//    })
-// }
+   const campaign = await deploy("CampaignFactory", {
+      from: deployer,
+      args: []
+   })
+}
