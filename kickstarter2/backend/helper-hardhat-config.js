d596da45ef3788b4ae3a2bd2eb2a615a51453a2a
LaupWing <loc_strict@hotmail.com>
2022-08-08 15:23:37 +0200

    update

--- kickstarter2/backend/helper-hardhat-config.js
+++ kickstarter2/backend/helper-hardhat-config.js
@@ -1,8 +1,11 @@
 const networkConfig = {
    4: {
       name: "rinkeby",
-      ethUsdPriceFeed: "0x0"
-   }
+      ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+   },
+   31337: {
+      name: "localhost"
+  },
 }
 
 const developmentChains = ["hardhat", "localhost"]
