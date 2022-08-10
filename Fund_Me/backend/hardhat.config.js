dc5bbbb4971a169553ad7060f23b65ad99007ccc
LaupWing <loc_strict@hotmail.com>
2022-08-10 15:07:55 +0200

    config

--- Fund_Me/backend/hardhat.config.js
+++ Fund_Me/backend/hardhat.config.js
@@ -2,5 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-deploy")
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+   solidity: "0.8.9",
+   networks:{
+
+   },
+   namedAccounts:{
+      deployer:{
+         default: 0,
+         4: 1, // rinkeby
+      }
+   }
 };
