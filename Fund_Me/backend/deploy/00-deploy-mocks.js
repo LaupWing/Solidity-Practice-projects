eef13a4075f35f1bc27fd9bbc457ee35f9150530
LaupWing <loc_strict@hotmail.com>
2022-08-16 18:30:26 +0200

    base deploy script

--- Fund_Me/backend/deploy/00-deploy-mocks.js
+++ Fund_Me/backend/deploy/00-deploy-mocks.js
@@ -1,18 +1,8 @@
 const {network} = require("hardhat")
-const { networkConfig } = require("../helper-hardhat-config")
 
 module.exports = async ({getNamedAccounts, deployments})=>{
    const {deploy, logs} = deployments
    const {deployer} = await getNamedAccounts()
    const chainId = network.config.chainId 
 
-   const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
-
-   const fundMe = await deploy("FundMe", {
-      from: deployer,
-      args: [
-         ethUsdPriceFeedAddress
-      ],
-      log: true
-   })
 }
