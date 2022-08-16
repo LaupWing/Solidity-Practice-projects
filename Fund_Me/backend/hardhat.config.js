bd19ff49908f31634479603bdac5eaa3d6786908
LaupWing <loc_strict@hotmail.com>
2022-08-16 18:29:05 +0200

    allrite

--- Fund_Me/backend/hardhat.config.js
+++ Fund_Me/backend/hardhat.config.js
@@ -2,7 +2,16 @@ require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-deploy")
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-   solidity: "0.8.9",
+   solidity: {
+      compilers:[
+         {
+            version:"0.8.9",
+         },
+         {
+            version:"0.6.0"
+         }
+      ]
+   },
    networks:{
 
    },
