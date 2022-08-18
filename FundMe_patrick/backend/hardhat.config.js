9567a66be98d1d8643ccb81c65be4b05947f6f46
LaupWing <loc_strict@hotmail.com>
2022-08-18 13:40:47 +0200

    update

--- FundMe_patrick/backend/hardhat.config.js
+++ FundMe_patrick/backend/hardhat.config.js
@@ -8,8 +8,6 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
 
-console.log(RINKEBY_RPC_URL)
-
 module.exports = {
    solidity: {
       compilers: [
