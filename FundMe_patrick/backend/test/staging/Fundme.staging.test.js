36aa5cfbc667dd25cfc32b18aa67e2fb3323dfd1
LaupWing <loc_strict@hotmail.com>
2022-08-20 14:31:46 +0200

    fundme staging

---
+++ FundMe_patrick/backend/test/staging/Fundme.staging.test.js
@@ -0,0 +1,14 @@
+const {getNamedAccounts, ethers, network} = require("hardhat")
+const { developmentChains } = require("../../helper-hardhat-config")
+
+
+developmentChains.includes(network.name) ? describe.skip : describe("FundMe", async ()=>{
+   let fundMe
+   let deployer
+   const sendValue = ethers.utils.parseEther("1")
+
+   beforeEach(async ()=>{
+      deployer = (await getNamedAccounts()).deployer
+      fundMe = await ethers.getContract("FundMe", deployer)
+   })
+}) 
