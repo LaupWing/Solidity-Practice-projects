add6b73d4fd8ec02ddd4fd436e212283f3ec2ca4
LaupWing <loc_strict@hotmail.com>
2022-08-20 14:39:05 +0200

    fund fundme

---
+++ FundMe_patrick/backend/scripts/fund.js
@@ -0,0 +1,18 @@
+const { getNamedAccounts, ethers } = require("hardhat")
+
+async function main (){
+   const {deployer} = await getNamedAccounts()
+   const fundMe = await ethers.getContract("FundMe", deployer)
+   console.log("Funding contract...")
+   const transationResponse = await fundMe.fund({value: ethers.utils.parseEther("1")})
+   await transationResponse.wait(1)
+   console.log("Funded!!")
+}
+
+
+main()
+   .then(()=> process.exit(0))
+   .catch((error)=>{
+      console.log(error)
+      process.exit(1)
+   })
