6b0de163fa6071554bb34fdc6b4a8994383aa556
LaupWing <loc_strict@hotmail.com>
2022-08-22 15:00:29 +0200

    update

--- Lottery_patrick/backend/contracts/Raffle.sol
+++ Lottery_patrick/backend/contracts/Raffle.sol
@@ -5,6 +5,7 @@ error Raffle__NotEnoughETHEntered();
 
 contract Raffle {
    uint256 private immutable i_entranceFee;
+   address payable[] private s_players;
 
    constructor(uint256 entranceFee){
       i_entranceFee = entranceFee;
@@ -18,5 +19,9 @@ contract Raffle {
 
    function getEntranceFee() public view returns(uint256){
       return i_entranceFee;
+   }
+
+   function getPlayer(uint256 index) public view returns(address){
+      return s_players[index];
    }
 }
