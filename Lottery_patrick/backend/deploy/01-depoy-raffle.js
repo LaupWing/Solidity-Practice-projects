2e65f095fe3687d8dd8e90b5b8b8b246a817e785
LaupWing <loc_strict@hotmail.com>
2022-08-25 11:52:08 +0200

    update

--- Lottery_patrick/backend/deploy/01-depoy-raffle.js
+++ Lottery_patrick/backend/deploy/01-depoy-raffle.js
@@ -1,22 +1,34 @@
 const {network, ethers} = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2")
+
 module.exports = async ({getNamedAccounts, deployments})=>{
    const {deploy , log} = deployments
    const {chainId} = network.config
    const {deployer} = await getNamedAccounts()
-   let vrfCoordinatorV2Address
+   let vrfCoordinatorV2Address, subscribionId
 
    if(developmentChains.includes(network.name)){
       const vrfCoordinatorV2Mock = await ethers.getContractAt("VRFCoordinatorV2Mock")
       vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+      const transationResponse = await vrfCoordinatorV2Mock.createSubscription()
+      const transationReceipt = await transationResponse.wait(1)
+      subscribionId = transationReceipt.events[0].args.subId
+
+      await vrfCoordinatorV2Mock.fundSubscription(subscribionId, VRF_SUB_FUND_AMOUNT)
    }else {
       vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
    }
+   
+   const {
+      raffleEntranceFee,
+      gasLane
+   } = networkConfig[chainId]
 
    const raffle = await deploy("Raffle", {
       from: deployer,
-      args: [],
+      args: [vrfCoordinatorV2Address, raffleEntranceFee, gasLane],
       log: true,
       waitConfirmations: network.config.blockConfirmations || 1
    })
--- Lottery_patrick/backend/helper-hardhat-config.js
+++ Lottery_patrick/backend/helper-hardhat-config.js
@@ -4,11 +4,13 @@ const networkConfig = {
    4 :{
       name: "rinkeby",
       vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
-      entranceFee: ethers.utils.parseEther("0.01")
+      raffleEntranceFee: ethers.utils.parseEther("0.01"),
+      gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
    },
    31337 :{
       name: "hardhat",
-      entranceFee: ethers.utils.parseEther("0.01")
+      raffleEntranceFee: ethers.utils.parseEther("0.01"),
+      gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
    },
 }
 
