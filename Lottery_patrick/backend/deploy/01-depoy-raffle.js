3ec1adaa6176f55454852a9771f4e69061b18459
LaupWing <loc_strict@hotmail.com>
2022-08-25 11:43:20 +0200

    hardahatconfig hardhat

--- Lottery_patrick/backend/deploy/01-depoy-raffle.js
+++ Lottery_patrick/backend/deploy/01-depoy-raffle.js
@@ -1,8 +1,18 @@
-const {network} = require("hardhat")
+const {network, ethers} = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 
 module.exports = async ({getNamedAccounts, deployments})=>{
    const {deploy , log} = deployments
+   const {chainId} = network.config
    const {deployer} = await getNamedAccounts()
+   let vrfCoordinatorV2Address
+
+   if(developmentChains.includes(network.name)){
+      const vrfCoordinatorV2Mock = await ethers.getContractAt("VRFCoordinatorV2Mock")
+      vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+   }else {
+      vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+   }
 
    const raffle = await deploy("Raffle", {
       from: deployer,
--- Lottery_patrick/backend/helper-hardhat-config.js
+++ Lottery_patrick/backend/helper-hardhat-config.js
@@ -1,8 +1,15 @@
+const { ethers } = require("hardhat")
+
 const networkConfig = {
    4 :{
       name: "rinkeby",
-      vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab"
-   }
+      vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+      entranceFee: ethers.utils.parseEther("0.01")
+   },
+   31337 :{
+      name: "hardhat",
+      entranceFee: ethers.utils.parseEther("0.01")
+   },
 }
 
 
