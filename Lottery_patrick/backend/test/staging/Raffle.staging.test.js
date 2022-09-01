44248a2fca5be1f9a211716fcaa5e479f6e4a6c0
LaupWing <loc_strict@hotmail.com>
2022-09-01 11:53:45 +0200

    update

--- Lottery_patrick/backend/test/staging/Raffle.staging.test.js
+++ Lottery_patrick/backend/test/staging/Raffle.staging.test.js
@@ -2,17 +2,14 @@ const { network, getNamedAccounts, deployments, ethers } = require("hardhat")
 const {developmentChains, networkConfig} = require("../../helper-hardhat-config")
 const {assert, expect} = require("chai")
 
-!developmentChains.includes(network.name) ? 
+developmentChains.includes(network.name) ? 
    describe.skip : 
    describe("Raffle Unit Tests", async ()=>{
-      let raffle, raffleEntranceFee, deployer, interval
+      let raffle, raffleEntranceFee, deployer
 
       beforeEach(async ()=>{
          deployer = (await getNamedAccounts()).deployer
-         await deployments.fixture(["all"])
          raffle = await ethers.getContract("Raffle", deployer)
-         vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
          raffleEntranceFee = await raffle.getEntranceFee()
-         interval = await raffle.getInterval()
       })
    })
