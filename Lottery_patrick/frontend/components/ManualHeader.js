d86b4f5dd1ea076734e4aec5c71eca803bd5ef53
LaupWing <loc_strict@hotmail.com>
2022-09-02 16:20:56 +0200

    check

--- Lottery_patrick/frontend/components/ManualHeader.js
+++ Lottery_patrick/frontend/components/ManualHeader.js
@@ -6,16 +6,21 @@ export default function ManualHeader(){
 
    useEffect(()=>{
       if(isWeb3Enabled) return
-      enableWeb3()
+      if(typeof window !== "undefined"){
+         if(window.localStorage.getItem("connected")){
+            enableWeb3()
+            // window.localStorage.setItem("connected", "inject")
+         }
+      }
    },[isWeb3Enabled])
 
    return(
       <div>
          {account ? (<div>Connected to {account.slice(0,6)}...{account.slice(account.length -4)}!</div>) : (<button onClick={async ()=>{ 
-            await enableWeb3()
             if(typeof window !== "undefined"){
                if(window.localStorage.getItem("connected")){
-                  window.localStorage.setItem("connected", "inject")
+                  await enableWeb3()
+                  // window.localStorage.setItem("connected", "inject")
                }
             }
          }}>
