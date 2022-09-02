09106204b117b67e9c9b854291f0e0ff22a73bd1
LaupWing <loc_strict@hotmail.com>
2022-09-02 16:29:55 +0200

    loggin in

--- Lottery_patrick/frontend/components/ManualHeader.js
+++ Lottery_patrick/frontend/components/ManualHeader.js
@@ -2,7 +2,7 @@ import { useEffect } from "react"
 import { useMoralis } from "react-moralis"
 
 export default function ManualHeader(){
-   const {enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3} = useMoralis()
+   const {enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3, isWeb3EnableLoading} = useMoralis()
 
    useEffect(()=>{
       if(isWeb3Enabled) return
@@ -25,12 +25,15 @@ export default function ManualHeader(){
 
    return(
       <div>
-         {account ? (<div>Connected to {account.slice(0,6)}...{account.slice(account.length -4)}!</div>) : (<button onClick={async ()=>{ 
-            await enableWeb3()
-            if(typeof window !== "undefined"){
-               window.localStorage.setItem("connected", "inject")
-            }
-         }}>
+         {account ? (<div>Connected to {account.slice(0,6)}...{account.slice(account.length -4)}!</div>) : (<button 
+            onClick={async ()=>{ 
+               await enableWeb3()
+               if(typeof window !== "undefined"){
+                  window.localStorage.setItem("connected", "inject")
+               }
+            }}
+            disabled={isWeb3EnableLoading}
+         >
             Connect
          </button>)}
          
