e670706d860a932c2abc4648bfff5adf34a78330
LaupWing <loc_strict@hotmail.com>
2022-09-03 14:23:58 +0200

    base constants

--- Lottery_patrick/frontend/components/LotteryEntrance.js
+++ Lottery_patrick/frontend/components/LotteryEntrance.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useWeb3Contract } from 'react-moralis'
 
 const LotteryEntrance = () => {
+   const {runContractFunction: enterRaffle} = useWeb3Contract({
+      
+   })
+
    return (
       <div>Hi from Lottery</div>
    )
