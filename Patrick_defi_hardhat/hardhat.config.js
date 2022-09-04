5b594aa43cd05f0dba1a1f159cf47b9eeeba7c43
LaupWing <loc_strict@hotmail.com>
2022-09-04 15:58:13 +0200

    hradhat config

--- Patrick_defi_hardhat/hardhat.config.js
+++ Patrick_defi_hardhat/hardhat.config.js
@@ -9,5 +9,50 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 module.exports = {
-   solidity: "0.8.9",
+   defaultNetwork: "hardhat",
+   networks:{
+      hardhat:{
+         chainId: 31337,
+         forking:{
+            url: MAINNET_RPC_URL
+         }
+      },
+      localhost:{
+         chainId: 31337
+      },
+      kovan:{
+         url: KOVAN_RPC_URL,
+         accounts: [PRIVATE_KEY],
+         chainId: 42,
+         blockConfirmations: 6
+      }
+   },
+   solidity: {
+      compilers:[
+         {
+            version: "0.8.9"
+         },
+         {
+            version: "0.6.12"
+         },
+         {
+            version: "0.4.19"
+         },
+      ]
+   },
+   etherscan:{
+      apiKey: ETHERSCAN_API_KEY
+   },
+   gasReporter:{
+      enabled: true,
+      currency: "USD",
+      outputFile: "gas-report.txt",
+      noColors: true
+   },
+   namedAccounts:{
+      deployer:{
+         default: 0,
+         1: 0
+      }
+   }
 };
