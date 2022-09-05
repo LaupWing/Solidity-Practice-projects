4528ade50c169b2996c280810e8af11e12a102f7
LaupWing <loc_strict@hotmail.com>
2022-09-05 15:16:19 +0200

    LendingPool

--- Patrick_defi_hardhat/hardhat.config.js
+++ Patrick_defi_hardhat/hardhat.config.js
@@ -8,7 +8,7 @@ const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL || "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
-
+// lendingPoolAddressesProvider: "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5"
 module.exports = {
    solidity: {
       compilers: [
@@ -29,7 +29,7 @@ module.exports = {
          chainId: 31337,
          forking: {
             url: MAINNET_RPC_URL
-         }
+         },
       },
       localhost: {
          chainId: 31337
--- Patrick_defi_hardhat/scripts/aaveBorrow.js
+++ Patrick_defi_hardhat/scripts/aaveBorrow.js
@@ -1,7 +1,14 @@
+const { getNamedAccounts } = require("hardhat")
 const { getWeth } = require("./getWeth")
 
 const main = async ()=>{
    await getWeth()
+   const {deployer} = await getNamedAccounts()
+
+}
+
+const getLendingPool = async ()=>{
+   
 }
 
 main()
