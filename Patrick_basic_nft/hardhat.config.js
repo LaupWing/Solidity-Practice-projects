eebfbe2f53033976394312634b991145d19d26bd
LaupWing <loc_strict@hotmail.com>
2022-09-06 13:37:37 +0200

    base

--- Patrick_basic_nft/hardhat.config.js
+++ Patrick_basic_nft/hardhat.config.js
@@ -3,6 +3,46 @@ require("@nomicfoundation/hardhat-toolbox")
 require("hardhat-deploy")
 require("dotenv").config()
 
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || process.env.ALCHEMY_MAINNET_RPC_URL || ""
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby.alchemyapi.io/v2/your-api-key"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+
 module.exports = {
-   solidity: "0.8.9",
+   solidity: {
+      compilers: [
+         {
+            version:"0.8.9"
+         }
+      ]
+   },
+   defaultNetwork: "hardhat",
+   networks:{
+      hardhat:{
+         chainId: 31337
+      },
+      rinkeby:{
+         url: RINKEBY_RPC_URL,
+         accounts: [PRIVATE_KEY],
+         chainId: 4,
+         blockConfirmations: 6
+      }
+   },
+   etherscan:{
+      apiKey: ETHERSCAN_API_KEY
+   },
+   gasReporter:{
+      enabled: true,
+      currency: "USD",
+      outputFile: "gas-report.txt",
+      noColors: true,
+      coinmarketcap: COINMARKETCAP_API_KEY
+   },
+   namedAccounts:{
+      deployer:{
+         default:0,
+         1: 0
+      }
+   }
 };
