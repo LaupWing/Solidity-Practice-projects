51cdc82203cf2a982b4f2a1a62534007d4e01b6c
LaupWing <loc_strict@hotmail.com>
2022-09-07 15:12:51 +0200

    constructoer

--- Patrick_nft/contracts/RandomIpfsNft.sol
+++ Patrick_nft/contracts/RandomIpfsNft.sol
@@ -5,7 +5,28 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 contract RandomIpfsNft is VRFConsumerBaseV2 {
-   function requestNft() public{}
+   VRFCoordinatorV2Interface private immutable i_vrfCoordinator; 
+   uint64 private immutable i_subscriptionId;
+   bytes32 private immutable i_gasLane;
+   uint32 private immutable i_callbackGasLimit;
+   uint16 private constant REQUEST_CONFIRMATIONS = 3;
+   uint32 private constant NUM_WORDS = 1;
+
+   constructor(
+      address vrfCoordinatorV2,
+      uint64 subscriptionId,
+      bytes32 gasLane,
+      uint32 callbackGasLimit
+   ) VRFConsumerBaseV2(vrfCoordinatorV2){
+      i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+      i_subscriptionId = subscriptionId;
+      i_gasLane = gasLane;
+      i_callbackGasLimit = callbackGasLimit;
+   }
+
+   function requestNft() public{
+
+   }
 
    function fulfillRandomWords(uint requestId, uint256[] memory randomWords) internal override{}
 
