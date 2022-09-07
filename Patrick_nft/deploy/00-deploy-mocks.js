a203c78a3dd652a713d45d0008d92431f113963a
LaupWing <loc_strict@hotmail.com>
2022-09-07 16:16:17 +0200

    00

---
+++ Patrick_nft/deploy/00-deploy-mocks.js
@@ -0,0 +1,27 @@
+const { network } = require("hardhat")
+const { DECIMALS, INITIAL_PRICE } = require("../helper-hardhat-config")
+
+const BASE_FEE = "250000000000000000"
+const GAS_PRICE_LINK = 1e9
+
+module.exports = async ({getNamedAccounts, deployments}) =>{
+   const {deploy, log} = deployments
+   const {deployer} = await getNamedAccounts()
+   const chainId = network.config.chainId
+
+   if(chainId == 31337){
+      log("Local network detected! Deploying mocks...")
+      await deploy("VRFCoordinatorV2Mock", {
+         from: deployer,
+         log: true,
+         args: [DECIMALS, INITIAL_PRICE]
+      })
+
+      log("Mocks deployed!!!")
+      log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+      log("You are deploying to local network, you'll need alcoal network running to interact")
+      log("!!!!!!!!!!!!")
+   }
+}
+
+module.exports.tags = ["all", "mocks", "main"]
