a232cbf0db9b955b78a4bde85dcfcfba51205af3
LaupWing <loc_strict@hotmail.com>
2022-09-07 15:04:19 +0200

    random ipfs

---
+++ Patrick_basic_nft/contracts/RandomIpfsNft.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.7;
+
+contract RandomIpfsNft {
+   function requestNft() public{}
+
+   function fulfillRandomWords(uint requestId, uint256[] memory randomWords) internal{}
+
+   function tokenURI(uint256) public {}
+}
