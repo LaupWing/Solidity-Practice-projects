aa50407159421d314a761827ab9258b66c42312a
LaupWing <loc_strict@hotmail.com>
2022-09-07 15:08:52 +0200

    added opencontracts

--- Patrick_nft/contracts/RandomIpfsNft.sol
+++ Patrick_nft/contracts/RandomIpfsNft.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.7;
 
-contract RandomIpfsNft {
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
+contract RandomIpfsNft is VRFConsumerBaseV2 {
    function requestNft() public{}
 
-   function fulfillRandomWords(uint requestId, uint256[] memory randomWords) internal{}
+   function fulfillRandomWords(uint requestId, uint256[] memory randomWords) internal override{}
 
    function tokenURI(uint256) public {}
 }
