5f3e16ff2edc5685095466f19d2d83e34dd6c6aa
LaupWing <loc_strict@hotmail.com>
2022-09-08 18:00:49 +0200

    storeImages

--- Patrick_nft/utils/uploadToPinata.js
+++ Patrick_nft/utils/uploadToPinata.js
@@ -2,11 +2,26 @@ const pinataSDK = require("@pinata/sdk")
 const path = require("path")
 const fs = require("fs")
 
+const pinataApiKey = process.env.PINATA_API_KEY || ""
+const pinataApiSecret = process.env.PINATA_API_SECRET || ""
+const pinata = pinataSDK(pinataApiKey, pinataApiSecret)
+
 async function storeImages(imagesFilePath){
    const fullImagesPath = path.resolve(imagesFilePath)
    const files = fs.readdirSync(fullImagesPath)
 
-   console.log(files)
+   let responses = []
+
+   for (fileIndex in files){
+      const readableStreamForFile = fs.createReadStream(`${fullImagesPath}/${files[fileIndex]}`)
+
+      try{
+         const response = await pinata.pinFileToIPFS(readableStreamForFile) 
+         responses.push(response)
+      }catch(error){
+         console.log(error)
+      }
+   }
 }
 
 module.exports = {
