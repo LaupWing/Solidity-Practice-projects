18143761815e5c486c4e55accf185f0a6a098e3e
LaupWing <loc_strict@hotmail.com>
2022-09-08 16:12:54 +0200

    update

--- Patrick_nft/deploy/02-deploy-random-nft.js
+++ Patrick_nft/deploy/02-deploy-random-nft.js
@@ -1,11 +1,33 @@
-const { network } = require("hardhat")
-const { developmentChains } = require("../helper-hardhat-config")
+const { network, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
 
 
 module.exports = async ({getNamedAccounts, deployments}) =>{
    const {deploy, log} = deployments
    const {deployer} = await getNamedAccounts()
+   const chainId = network.config.chainId
 
-   
+   let vrfCoordinatorV2Address, subscribtionId
+
+   if(developmentChains.includes(network.name)){
+      const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+      vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+      const tx = await vrfCoordinatorV2Mock.createSubscription()
+      const txReceipt = await tx.wait(1)
+      subscribtionId = txReceipt.events[0].args.subId
+      
+   }else{
+      vrfCoordinatorV2Address =  networkConfig[chainId].vrfCoordinatorV2
+      subscribtionId =  networkConfig[chainId].subscribtionId
+   }
+   log("--------------------")
+   const args = [
+      vrfCoordinatorV2Address, 
+      subscribtionId,
+      networkConfig[chainId].gasLane,
+      networkConfig[chainId].callbackGasLimit,
+      // tokenUri
+      networkConfig[chainId].mintFee,
+   ]
 }
