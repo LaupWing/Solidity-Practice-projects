d2c869549bb23da8421686672620f597a1099171
LaupWing <loc_strict@hotmail.com>
2022-09-09 13:55:42 +0200

    fulfillRandomWords

--- Patrick_nft/test/randomIpfsNft.test.js
+++ Patrick_nft/test/randomIpfsNft.test.js
@@ -41,4 +41,35 @@ const { developmentChains } = require("../helper-hardhat-config")
                .to.emit(randomIpfsNft, "NftRequested")
          })
       })
+
+      describe("fulfillRandomWords", ()=>{
+         if("mints NFT after random number is returned", async()=>{
+            await new Promise(async (resolve,reject)=>{
+               randomIpfsNft.once("NftMinted", async ()=>{
+                  try{
+                     const tokenUri = await randomIpfsNft.tokenURI("0")
+                     const tokenCounter = await randomIpfsNft.getTokenCounter()
+                     assert.equal(tokenUri.toString().includes("ipfs://"), true)
+                     assert.equal(tokenCounter.toString(), "1")
+                     resolve()
+                  }catch(e){
+                     reject(e)
+                  }
+               })
+               try{
+                  const fee = await randomIpfsNft.getMintFee()
+                  const requestNftResponse = await randomIpfsNft.requestNft({
+                     value: fee.toString()
+                  })
+                  const requestNftReceipt = await requestNftResponse.wait(1)
+                  await vrfCoordinatorV2Mock.fulfillRandomWords(
+                     requestNftReceipt.events[1].args.requestId,
+                     randomIpfsNft.address
+                  )
+               }catch(e){
+                  reject(e)
+               }
+            })
+         })
+      })
    })
