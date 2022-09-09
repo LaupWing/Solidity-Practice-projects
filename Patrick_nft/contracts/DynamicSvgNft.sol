613fb56989acd1cb62a668b5b456794db99ab413
LaupWing <loc_strict@hotmail.com>
2022-09-09 14:14:00 +0200

    base64EncodedSvgPrefix

--- Patrick_nft/contracts/DynamicSvgNft.sol
+++ Patrick_nft/contracts/DynamicSvgNft.sol
@@ -5,9 +5,18 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract DynamicSvgNft is ERC721 {
    uint256 private s_tokenCounter;
+   string private i_lowImageURI;
+   string private i_highImageURI;
+   string private constant base64EncodedSvgPrefix = "data:image/svg+xml;base64";
 
    constructor(string memory lowSvg, string memory highSvg) ERC721("Dynamic SVG NFT", "DSN"){
       s_tokenCounter = 0;
+      i_lowImageURI = lowSvg;
+      i_highImageURI = highSvg;
+   }
+
+   function svgToImageURI() public pure returns(string memory){
+      
    }
 
    function mintNft() public{
