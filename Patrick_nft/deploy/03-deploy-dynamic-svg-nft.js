b1d0bc6e3cac170a39fb5eb69039b57bfdd7c0b7
LaupWing <loc_strict@hotmail.com>
2022-09-11 15:06:35 +0200

    udpate

--- Patrick_nft/deploy/03-deploy-dynamic-svg-nft.js
+++ Patrick_nft/deploy/03-deploy-dynamic-svg-nft.js
@@ -1,10 +1,20 @@
-const { network } = require("hardhat")
-const { developmentChains } = require("../helper-hardhat-config")
+const { network, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
 
 module.exports = async ({getNamedAccounts, deployments}) =>{
    const {deploy, log} = deployments
    const {deployer} = await getNamedAccounts()
+
+   const chainId = network.config.chainId
+   let ethUsdPriceFeedAddress
+
+   if(developmentChains.includes(network.name)){
+      const EthUsdAggregator = await ethers.getContract("MockV3Aggregator")
+      ethUsdPriceFeedAddress = EthUsdAggregator.address
+   }else{
+      ethUsdPriceFeedAddress = networkConfig[chainId].ethUsdPriceFeed
+   }
 
    log("---------------------")
 }
