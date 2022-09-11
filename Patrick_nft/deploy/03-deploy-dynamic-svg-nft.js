60825c9bbe823e197aa152ed87a3e58931a689eb
LaupWing <loc_strict@hotmail.com>
2022-09-11 15:11:37 +0200

    can delpoy

--- Patrick_nft/deploy/03-deploy-dynamic-svg-nft.js
+++ Patrick_nft/deploy/03-deploy-dynamic-svg-nft.js
@@ -1,6 +1,7 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
+const fs = require("fs")
 
 module.exports = async ({getNamedAccounts, deployments}) =>{
    const {deploy, log} = deployments
@@ -16,7 +17,21 @@ module.exports = async ({getNamedAccounts, deployments}) =>{
       ethUsdPriceFeedAddress = networkConfig[chainId].ethUsdPriceFeed
    }
 
+   const lowSVG = await fs.readFileSync("./images/dynamic/frown.svg", {encoding: "utf-8"}) 
+   const highVG = await fs.readFileSync("./images/dynamic/happy.svg", {encoding: "utf-8"}) 
    log("---------------------")
+   const args = [ethUsdPriceFeedAddress, lowSVG, highVG]
+   const dynamicSvgNFt = await deploy("DynamicSvgNft", {
+      from: deployer,
+      args,
+      log:true,
+      waitConfirmations: network.config.blockConfirmations || 1
+   })
+   if(!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY){
+      log("Verifying...")
+      await verify(dynamicSvgNFt.address, args)
+      log("-------------------------------------")
+   }
 }
 
-module.exports.tags =["all", "basicnft", "main"]
+module.exports.tags =["all", "dyanmicsvg", "main"]
