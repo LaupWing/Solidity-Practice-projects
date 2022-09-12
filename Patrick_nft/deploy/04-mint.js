0a94d19638ca3a47fa7993e69c3d4126cf6533fd
LaupWing <loc_strict@hotmail.com>
2022-09-12 12:18:45 +0200

    deploy

--- Patrick_nft/deploy/04-mint.js
+++ Patrick_nft/deploy/04-mint.js
@@ -1,4 +1,5 @@
 const {ethers, network} = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
 
 
 module.exports = async ({getNamedAccounts})=>{
@@ -11,7 +12,26 @@ module.exports = async ({getNamedAccounts})=>{
 
    const randomIpfs = await ethers.getContract("RandomIpfsNft", deployer)
    const mintFee =await randomIpfs.getMintFee()
-   const randomIpfsMint = await randomIpfs.requestNft({value: mintFee.toString()})
 
-   
+   await new Promise(async (resolve, reject)=>{
+      setTimeout(resolve, 300000)
+      randomIpfs.once("NftMinted", async ()=>{
+         resolve()
+      })
+      const randomIpfsMintTx = await randomIpfs.requestNft({value: mintFee.toString()})
+      const randomIpfsMintTxReceipt = await randomIpfsMintTx.wait(1)
+
+      if(developmentChains.includes(network.name)){
+         const requestId = randomIpfsMintTxReceipt.events[1].args.requestId.toString()
+         const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+         await vrfCoordinatorV2Mock.fulfillRandomWords(requestId, randomIpfs.address)
+      }
+   })
+   console.log(`Random IPFS NFT index 0 tokenURI ${await randomIpfs.tokenURI()}`)
+
+   const highValue = ethers.utils.parseEther("4000")
+   const dynamicSvgNft = await ethers.getContract("DynamicSvgNft", deployer)
+   const dynamicSvgNftMintTx = await dynamicSvgNft.mintNft(highValue.toString())
+   await dynamicSvgNftMintTx.wait(1)
+   console.log(`Dynamic SVG NFT index 0 tokenURI ${await dynamicSvgNft.tokenURI(0)}`)
 }
