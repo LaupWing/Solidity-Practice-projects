8d7dc7367f1fbeff857029e2a433a135e6cf7adb
LaupWing <loc_strict@hotmail.com>
2022-09-12 13:01:24 +0200

    ItemListed

--- Patrick_NftMarketplace/backend_hardhat/contracts/NftMarketplace.sol
+++ Patrick_NftMarketplace/backend_hardhat/contracts/NftMarketplace.sol
@@ -1,9 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
 error NftMarketplace__PriceMustBeAboveZero();
+error NftMarketplace__NotApprovedForMarketplace();
 
 contract NftMarketplace{
+
+   event ItemListed(
+      address indexed seller,
+      address indexed nftAddress,
+      uint256 indexed tokenId,
+      uint256 price
+   );
+
+   struct Listing {
+      uint256 price;
+      address seller;
+   }
+   // NFT Contract address -> NFT TokenId -> Listing
+   mapping(address => mapping(uint256 => Listing)) private s_listings;
 
    function listItem(
       address nftAddress, 
@@ -13,5 +30,12 @@ contract NftMarketplace{
       if(price <= 0) {
          revert NftMarketplace__PriceMustBeAboveZero();
       }   
+
+      IERC721 nft = IERC721(nftAddress);
+      if(nft.getApproved(tokenId) != address(this)){
+         revert NftMarketplace__NotApprovedForMarketplace();
+      }
+      s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
+      emit ItemListed(msg.sender, nftAddress, tokenId, price);
    }
 }
