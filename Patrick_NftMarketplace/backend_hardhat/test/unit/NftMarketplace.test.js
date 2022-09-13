aa3fc7aa32a90c8566c845c79ae49f3764ab1dc0
LaupWing <loc_strict@hotmail.com>
2022-09-13 13:01:13 +0200

    update

--- Patrick_NftMarketplace/backend_hardhat/test/unit/NftMarketplace.test.js
+++ Patrick_NftMarketplace/backend_hardhat/test/unit/NftMarketplace.test.js
@@ -8,16 +8,45 @@ const {developmentChains} = require("../../helper-hardhat-config")
    const TOKEN_ID = 0
 
    beforeEach(async()=>{
-      deployer = (await getNamedAccounts()).deployer
       const accounts = await ethers.getSigners()
+      deployer = accounts[0]
       player = accounts[1]
       await deployments.fixture(["all"])
-      nftMarketplace = await ethers.getContract("NftMarketplace")
+      nftMarketplaceContract = await ethers.getContract("NftMarketplace")
+      nftMarketplace = nftMarketplaceContract.connect(deployer)
       basicNft = await ethers.getContract("BasicNft")
       await basicNft.mintNft()
       await basicNft.approve(nftMarketplace.address, TOKEN_ID)
    })
 
+   describe("listItem", ()=>{
+      it("emits an event after listing an item", async ()=>{
+         expect(await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE).to.emit("ItemListed"))
+      })
+
+      it("exclusively items that haven't been listed", async ()=>{
+         await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
+         const error = `AlreadyListed("${basicNft.address}", ${TOKEN_ID})`
+
+         await expect(
+            nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
+         ).to.be.revertedWith(error)
+      })
+
+      it("needs approvals to list item", async ()=>{
+         await basicNft.approve(ethers.constants.AddressZero, TOKEN_ID)
+         await expect(
+            nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
+         ).to.be.revertedWith("NotApprovedForMarketplace")
+      })
+
+      it("Updates listing with seller and price", async ()=>{
+         await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
+         const listing = await nftMarketplace.getListing(basicNft.address, TOKEN_ID)
+         assert(listing.price.toString() == PRICE.toString())
+         assert(listing.seler.toString() == deployer.address)
+      })
+   })
    it("lists and can be bought", async ()=>{
       await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
       const playerConnectedNftMarketplace = nftMarketplace.connect(player)
@@ -28,4 +57,5 @@ const {developmentChains} = require("../../helper-hardhat-config")
       assert(newOwner.toString() == player.address)
       assert(deployerProceeds.toString() ==  PRICE.toString())
    })
+   
 })
