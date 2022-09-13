55aaade8ebe21b8b148b71491a787161cbcd3520
LaupWing <loc_strict@hotmail.com>
2022-09-13 12:14:36 +0200

    withdrawProceeds

--- Patrick_NftMarketplace/backend_hardhat/contracts/NftMarketplace.sol
+++ Patrick_NftMarketplace/backend_hardhat/contracts/NftMarketplace.sol
@@ -15,7 +15,9 @@ error NftMarketplace__NotListed(
    address nftAddress,
    uint256 tokenId
 );
+error NftMarketplace__NoProceeds();
 error NftMarketplace__NotOwner();
+error NftMarketplace__TransferFailed();
 error NftMarketplace__AlreadyListed(
    address nftAddress,
    uint256 tokenId
@@ -136,5 +138,18 @@ contract NftMarketplace is ReentrancyGuard{
    {
       s_listings[nftAddress][tokenId].price = newPrice;
       emit ItemListed(msg.sender, nftAddress, tokenId, newPrice);
+   }
+
+   function withdrawProceeds() external {
+      uint256 proceeds = s_proceeds[msg.sender];
+      if(proceeds <= 0){
+         revert NftMarketplace__NoProceeds();
+      }
+      s_proceeds[msg.sender] = 0;
+      (bool success, ) = payable(msg.sender).call{value: proceeds}("");
+
+      if(!success){
+         revert NftMarketplace__TransferFailed();
+      }
    }
 }
