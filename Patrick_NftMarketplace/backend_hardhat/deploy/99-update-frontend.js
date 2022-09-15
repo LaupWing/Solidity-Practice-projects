6c4c0a735c8f5390807e5b834585fbb20644b945
LaupWing <loc_strict@hotmail.com>
2022-09-15 09:15:05 +0200

    wrapped

--- Patrick_NftMarketplace/backend_hardhat/deploy/99-update-frontend.js
+++ Patrick_NftMarketplace/backend_hardhat/deploy/99-update-frontend.js
@@ -4,28 +4,31 @@ const fs = require("fs")
 const frontEndContractsFile = "../nextjs-nft-marketplace/constants/networkMapping.json"
 
 module.exports = async ()=>{
-   console.log("heh")
-   console.log(process.env.UPDATE_FRONTEND)
    if(process.env.UPDATE_FRONTEND){
       updateContractAddresses()
    }
 }
 
 async function updateContractAddresses(){
-   const nftMarketplace = await ethers.getContract("NftMarketplace")
-   const chainId = network.config.chainId.toString()
-   const contractAddresses = JSON.parse(fs.readFileSync(frontEndContractsFile, "utf-8"))
-
-   if(chainId in contractAddresses){
-      if(!contractAddresses[chainId]["NftMarketplace"].includes(nftMarketplace.address)){
-         contractAddresses[chainId]["NftMarketplace"].push(nftMarketplace.address)
-      }else{
-         contractAddresses[chainId] = {
-            NftMarketplace: [nftMarketplace.address]
+   try{
+      const nftMarketplace = await ethers.getContract("NftMarketplace")
+      console.log(nftMarketplace)
+      const chainId = network.config.chainId.toString()
+      const contractAddresses = JSON.parse(fs.readFileSync(frontEndContractsFile, "utf-8"))
+   
+      if(chainId in contractAddresses){
+         if(!contractAddresses[chainId]["NftMarketplace"].includes(nftMarketplace.address)){
+            contractAddresses[chainId]["NftMarketplace"].push(nftMarketplace.address)
+         }else{
+            contractAddresses[chainId] = {
+               NftMarketplace: [nftMarketplace.address]
+            }
          }
       }
+      fs.writeFileSync(frontEndContractsFile, JSON.stringify(contractAddresses))
+   }catch(e){
+      console.log(e)
    }
-   fs.writeFileSync(frontEndContractsFile, JSON.stringify(contractAddresses))
 }
 
 
