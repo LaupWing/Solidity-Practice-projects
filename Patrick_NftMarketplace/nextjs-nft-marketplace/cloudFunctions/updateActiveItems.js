af9d55f24ccd2e5025e2366dada4bb21738bfffb
LaupWing <loc_strict@hotmail.com>
2022-09-16 11:40:06 +0200

    item canceld

--- Patrick_NftMarketplace/nextjs-nft-marketplace/cloudFunctions/updateActiveItems.js
+++ Patrick_NftMarketplace/nextjs-nft-marketplace/cloudFunctions/updateActiveItems.js
@@ -22,3 +22,19 @@ Moralis.Cloud.afterSave("ItemListed", async (request)=>{
       await activeItem.save()
    }
 })
+
+Moralis.Cloud.afterSave("ItemCanceled", async request =>{
+   const confirmed = request.object.get("confirmed")
+   const logger = Moralis.Cloud.getLogger()
+   logger.info(`Marketplace | Object ${request.object}`)
+
+   if(confirmed){
+      const ActiveItem = Moralis.Object.extend("ActiveItem")
+      const query = new Moralis.Query(ActiveItem)
+
+      query.equalTo("marketplaceAddress", request.object.get("address"))
+      query.equalTo("nftAddress", request.object.get("nftAddress"))
+      query.equalTo("tokenId", request.object.get("tokenId"))
+   }
+
+})
