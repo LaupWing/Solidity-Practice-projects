4b1bf445a20f9e86b50e06ce6379bd800b487c45
LaupWing <loc_strict@hotmail.com>
2022-09-16 11:12:18 +0200

    hmm

--- Patrick_NftMarketplace/nextjs-nft-marketplace/cloudFunctions/updateActiveItems.js
+++ Patrick_NftMarketplace/nextjs-nft-marketplace/cloudFunctions/updateActiveItems.js
@@ -12,11 +12,11 @@ Moralis.Cloud.afterSave("ItemListed", async (request)=>{
       const ActiveItem = Moralis.Object.extend("ActiveItem")
 
       const activeItem = new ActiveItem()
-      activeItem.set("marketplaceAddress", request.object.get("address"))
-      activeItem.set("nftAddress", request.object.get("nftAddress"))
-      activeItem.set("price", request.object.get("price"))
-      activeItem.set("tokenId", request.object.get("tokenId"))
-      activeItem.set("seller", request.object.get("seller"))
+      // activeItem.set("marketplaceAddress", request.object.get("address"))
+      // activeItem.set("nftAddress", request.object.get("nftAddress"))
+      // activeItem.set("price", request.object.get("price"))
+      // activeItem.set("tokenId", request.object.get("tokenId"))
+      // activeItem.set("seller", request.object.get("seller"))
       logger.info(`Adding Address ${request.object.get("address")}. TokenId: ${request.boject.get("tokenId")}`)
       logger.info("Saving")
 
