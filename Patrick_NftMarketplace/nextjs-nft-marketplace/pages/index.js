600cf12bc742886905c39055d431976d800a01a5
LaupWing <loc_strict@hotmail.com>
2022-09-18 13:07:04 +0200

    fetcing

--- Patrick_NftMarketplace/nextjs-nft-marketplace/pages/index.js
+++ Patrick_NftMarketplace/nextjs-nft-marketplace/pages/index.js
@@ -1,9 +1,23 @@
+import { useMoralisQuery } from 'react-moralis'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+   const {data: listedNfts, isFetching: fetchingListedNfts} = useMoralisQuery(
+      "ActiveItem",
+      (query) => query.limit(10).descending("tokenId")
+   )
+
    return (
       <div className={styles.container}>
-         
+         {fetchingListedNfts ?  
+            (<div>Loading...</div>) : 
+            listedNfts.map((nft) => {
+               console.log(nft.attributes)
+               const {price, nftAddress, tokenId, marketplaceAddress, seller} = nft.attributes
+               return (
+                  <div>Price: {price}. NftAddress: {nftAddress}. TokenId: {tokenId}. Seller: {seller}</div>
+               )
+            })}     
       </div>
    )
 }
