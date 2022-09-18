7df4b5295b914480dd23a9853967fe4d92937264
LaupWing <loc_strict@hotmail.com>
2022-09-18 13:35:11 +0200

    NFT BOX

--- Patrick_NftMarketplace/nextjs-nft-marketplace/components/NFTBox.js
+++ Patrick_NftMarketplace/nextjs-nft-marketplace/components/NFTBox.js
@@ -1,16 +1,30 @@
-import React, { useState } from 'react'
-import { useWeb3Contract } from 'react-moralis'
+import React, { useEffect, useState } from 'react'
+import { useMoralis, useWeb3Contract } from 'react-moralis'
+import nftMarketplaceAbi from "../constants/NftMarketplace.json"
+import basicNftAbi from "../constants/BasicNft.json"
 
 const NFTBox = ({price, nftAddress, seller, marketplaceAddress, seller}) => {
    const [imageURI, setImageURI] = useState("")
+   const {isWeb3Enabled} = useMoralis()
 
    const {runContractFunction: getTokenURI} = useWeb3Contract({
-      abi
+      abi: basicNftAbi,
+      contractAddress: nftAddress,
+      functionName: "tokenURI",
+      params:{
+         tokenId
+      }
    })
 
    const updateUI = async ()=>{
-
+      const tokenUri = await getTokenURI()
    }
+
+   useEffect(()=>{
+      if(isWeb3Enabled){
+         updateUI()
+      }
+   }, [isWeb3Enabled])
 
    return (
       <div>NFTBox</div>
