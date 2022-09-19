8858fec561fd6586e548a1214dabcd4a419e6ec4
LaupWing <loc_strict@hotmail.com>
2022-09-19 12:34:44 +0200

    handleApproveSuccess

--- Patrick_NftMarketplace/nextjs-nft-marketplace/pages/sell-nft.js
+++ Patrick_NftMarketplace/nextjs-nft-marketplace/pages/sell-nft.js
@@ -1,12 +1,74 @@
+import { ethers } from 'ethers'
 import Head from 'next/head'
 import Image from 'next/image'
 import { Form } from 'web3uikit'
 import styles from '../styles/Home.module.css'
 
+import networkMapping from "../constants/networkMapping.json"
+import basicNftAbi from "../constants/BasicNft.json"
+import marketplaceAbi from "../constants/NftMarketplace.json"
+import { useMoralis, useWeb3Contract } from 'react-moralis'
+
 export default function Home() {
+   const {chainId} = useMoralis()
+
+   const chainString = chainId ? parseInt(chainId).toString() : "31337"
+   const marketplaceAddress = networkMapping[chainId].NftMarketplace[0]
+
+   const {runContractFunction} = useWeb3Contract()
+
+   const approveAndList = async (data)=>{
+      console.log("Approving")
+
+      const nftAddress = data.data[0].inputResult
+      const tokenId = data.data[1].inputResult
+      const price = ethers.utils.parseUnits(data.data[2].inputResult, "ether").toString()
+
+      const approveOptions = {
+         abi:basicNftAbi,
+         contractAddress: nftAddress,
+         function: "approve",
+         params:{
+            to: marketplaceAddress,
+            tokenId
+         }
+      }
+
+      await runContractFunction({
+         params: approveOptions,
+         onSuccess: ()=>handleApproveSuccess(nftAddress, tokenId, price),
+         onError: (error)=> console.error(error)
+      })
+   }
+
+   const handleApproveSuccess = async (nftAddress, tokenId, price)=>{
+      console.log("Ok! Now time to")
+      const listOptions ={
+         abi:marketplaceAbi,
+         contractAddress: marketplaceAddress,
+         function: "listItem",
+         params:{
+            nftAddress, 
+            tokenId,
+            price
+         }
+      }
+      
+      await runContractFunction({
+         params: listOptions,
+         onSuccess: ()=> handleListSuccess(),
+         onError: error => console.error(error)
+      })
+   }
+
+   const handleListSuccess = () =>{
+      
+   }
+
    return (
       <div className={styles.container}>
          <Form
+            onSubmit={approveAndList}
             data={[
                {
                   name: "NFT Address",
